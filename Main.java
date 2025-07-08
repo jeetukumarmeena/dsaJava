26e4a9374a5d92c2bc24dfa2e24f959877065c74
jeetu kumar meena <mr.jeetumeena066@gmail.com>
2025-07-08 22:07:34 +0530

    static  keyword  defined

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) { // the use of the static keyword is to indicate that the main method is a class method and can be called without creating an instance of the class
+        System.out.println("hello world");
+    }
+}
